2155b3ea18434f55b65f977004657fe936ed1c26
mcchukwu <miraclechukwu2011@gmail.com>
2024-12-17 19:09:52 +0100

    initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,10 @@
 module.exports = {
+  purge: false,
+  content: [
+    './public/**/*.{js,tsx,html,css}',
+    'tailwind.config.js',
+    './public/**/*.d.ts',
+  ],
   theme: {
     fontFamily: {
       'sans': 'Inter, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Helvetica Neue, Arial, Noto Sans, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji',
